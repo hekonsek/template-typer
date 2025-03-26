b7976b42779b7b8141e56a7e9e40b7d941a47c38
Henryk Konsek <hekonsek@gmail.com>
2025-03-26 12:55:48 +0100

    Added termcolor.

--- template_typer/cli.py
+++ template_typer/cli.py
@@ -1,10 +1,11 @@
 import typer
+from termcolor import colored
 
 # Root command
 
 app = typer.Typer(no_args_is_help=True)
 
-# Some subcommands section 
+# Subcommands 
 
 app_some_command = typer.Typer(no_args_is_help=True)
 app.add_typer(app_some_command, name="some-command")
@@ -12,6 +13,12 @@ app.add_typer(app_some_command, name="some-command")
 @app_some_command.command(name="subcommand")
 def subcommand():
     typer.echo("Hello world!")
+
+# Colors
+
+@app.command()
+def colors():
+    typer.echo(f"I'm {colored("green text", "green")}!")
 
 # Main section
 
